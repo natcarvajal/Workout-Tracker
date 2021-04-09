80fe7e34b56515e03c08cc8b6613234f6c1e8d9e
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-04-09 01:11:50 -0400

    after tutoring session/almost got the routes done.. hopefully

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,21 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.get("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
     })
     .catch((err) => {
-      res.status(400).json(err);
+      res.json(err);
     });
 });
 
@@ -30,7 +38,6 @@ router.post("/api/workouts", ({ body }, res) => {
       res.status(400).json(err);
     });
 });
-
 router.get("/api/workouts/range", (req, res) => {
   Workout.find({})
     .sort({ date: -1 })
