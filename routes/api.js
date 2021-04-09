39e6c3d56314b2892bff1aab83caf41909985c04
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-04-09 01:28:22 -0400

    routes are in place but not working.. sick

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,26 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body }, res) => {
+  Workout.insertMany(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
 router.get("/api/workouts", (req, res) => {
   Workout.aggregate([
     {
@@ -19,25 +39,6 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-router.put("/api/workouts/:id", ({ body }, res) => {
-  Workout.insertMany(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-router.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
 router.get("/api/workouts/range", (req, res) => {
   Workout.aggregate([
     {
